8166398780485ee0d784c8a6a07216d067f9b589
Gabriel <silva.gabrielmelosantos@gmail.com>
2023-07-05 07:04:10 -0300

    apostaService com try catch

--- src/main/java/com/gabriel/aposta/services/ApostaService.java
+++ src/main/java/com/gabriel/aposta/services/ApostaService.java
@@ -1,10 +1,13 @@
 package com.gabriel.aposta.services;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.gabriel.aposta.model.Aposta;
 import com.gabriel.aposta.model.Apostador;
@@ -18,16 +21,25 @@ public class ApostaService {
 
 	@Autowired
 	private ApostadorRepository apostadorRepository;
-	
+
 	@Autowired
 	private ApostaRepository apostaRepository;
-	
+
 	public ApostaOut salvaAposta( ApostaIn apostaIn) {
+		ApostaOut apostaOut = null;
+		
+	try {
 		Optional<Apostador> apostador = apostadorRepository.findById(apostaIn.getIdApostador());
 		String numAposta = UUID.randomUUID().toString();
 		Aposta aposta = new Aposta(numAposta, apostador.get());
-		apostaRepository.save(aposta);
 		
-		return new ApostaOut(numAposta, apostador.get().getNome(), apostador.get().getEmail());
+		apostaRepository.save(aposta);
+		apostaOut = new ApostaOut (numAposta, apostador.get().getNome(), apostador.get().getEmail());
+		
+	}catch(NoSuchElementException exception) {
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+	}
+	
+	return apostaOut;
 	}
 }
